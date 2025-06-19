716634f4944e5b741eb14f68648390d1f00422e7
rick <roman20071999@gmail.com>
2025-06-19 18:47:34 +0500

    poihali 2

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,33 @@
 terraform {
   required_version = ">= 1.8.4"
+
+  backend "s3" {
+
+    shared_credentials_files = ["~/.aws/credentials.ini"]
+    #shared_config_files      = ["~/.aws/config"]
+    profile = "default"
+    region  = "ru-central1"
+
+    bucket = "my-demo-bucket" #FIO-netology-tfstate
+    key    = "production/terraform.tfstate"
+
+
+    # access_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+    # secret_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+    endpoints = {
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g4rrcf3jq0fkd1d7h1/etn25r36t2r01fp1iv7q"
+      s3       = "https://storage.yandexcloud.net"
+    }
+
+    dynamodb_table = "lock-prod"
+  }
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
