1c611a399cb7e2dce3ac398d9c40535851cac1f5
rick <roman20071999@gmail.com>
2025-06-19 22:53:39 +0500

    poihali 2

--- infra/providers.tf
+++ infra/providers.tf
@@ -36,7 +36,8 @@ terraform {
   }
 }
 provider "yandex" {
-  service_account_key_file = file("~/.key.json")
+  #service_account_key_file = file("~/.key.json")
+  service_account_key_file = base64decode(var.sa_key_b64)
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
