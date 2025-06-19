8afd2974db4200d80c563bd20569c2b3165b662a
rick <roman20071999@gmail.com>
2025-06-20 00:05:28 +0500

    poihali 2

--- infra/main.tf
+++ infra/main.tf
@@ -54,12 +54,12 @@ resource "yandex_compute_instance" "web" {
     folder_id   = var.folder_id
     zone        = var.zone
 
-    DB_HOST          = data.yandex_lockbox_secret_version.db_host.data["value"]
-    DB_USER          = data.yandex_lockbox_secret_version.db_user.data["value"]
-    DB_PASSWORD      = data.yandex_lockbox_secret_version.db_password.data["value"]
-    DB_DATABASE      = data.yandex_lockbox_secret_version.db_database.data["value"]
-    DB_TABLE         = data.yandex_lockbox_secret_version.db_table.data["value"]
-    DB_ROOT_PASSWORD = data.yandex_lockbox_secret_version.db_root_password.data["value"]
+    DB_HOST          = data.yandex_lockbox_secret_version.db_host.payload["value"]
+    DB_USER          = data.yandex_lockbox_secret_version.db_user.payload["value"]
+    DB_PASSWORD      = data.yandex_lockbox_secret_version.db_password.payload["value"]
+    DB_DATABASE      = data.yandex_lockbox_secret_version.db_database.payload["value"]
+    DB_TABLE         = data.yandex_lockbox_secret_version.db_table.payload["value"]
+    DB_ROOT_PASSWORD = data.yandex_lockbox_secret_version.db_root_password.payload["value"]
 
     ssh-keys           = "ubuntu:${file(var.ssh_path)}"
     user-data          = data.template_file.cloudinit.rendered
