cd2b85499ba93b82a808954ee3ad21cefa8a838a
rick <roman20071999@gmail.com>
2025-06-24 21:52:39 +0500

    some fix

--- infra/output.tf
+++ infra/output.tf
@@ -5,3 +5,15 @@ output "web_sg_id" {
 output "web_sg_name" {
   value = yandex_vpc_security_group.web.name
 }
+
+output "db_address" {
+  value = yandex_mdb_mysql_cluster.db.host[0].fqdn
+}
+
+output "db_port" {
+  value = yandex_mdb_mysql_cluster.db.port
+}
+
+output "registry_url" {
+  value = yandex_container_registry.develop_registry.repository_id
+}
