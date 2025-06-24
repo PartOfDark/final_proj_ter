6f2c1b80c25894272a6237853a44d18cf4b93ba3
rick <roman20071999@gmail.com>
2025-06-24 23:43:20 +0500

    just check what it is

--- infra/output.tf
+++ infra/output.tf
@@ -5,3 +5,15 @@ output "web_sg_id" {
 output "web_sg_name" {
   value = yandex_vpc_security_group.web.name
 }
+
+output "db_host" {
+  value = yandex_mdb_mysql_cluster.db.host
+}
+
+output "registry_id" {
+  value = yandex_container_registry.develop_registry.id
+}
+
+output "registry_url" {
+  value = "cr.yandex/${yandex_container_registry.develop_registry.id}"
+}
